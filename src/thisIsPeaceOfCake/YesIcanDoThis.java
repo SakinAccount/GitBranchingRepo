af197c0fcda9da2f556a828cf1a4ce9e2a128655
parviz <pamir0986@gmail.com>
2022-11-09 18:49:34 -0500

    this is for branching purpose

---
+++ src/thisIsPeaceOfCake/YesIcanDoThis.java
@@ -0,0 +1,10 @@
+package thisIsPeaceOfCake;
+
+public class YesIcanDoThis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I learn this or i will fast for the rest of the night");
+	}
+
+}
