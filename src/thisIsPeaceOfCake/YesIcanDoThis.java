e31a4f63eb1d19fd718052a0a3ce0207959f489b
parviz <pamir0986@gmail.com>
2022-11-09 19:10:05 -0500

    this is on practice branch

--- src/thisIsPeaceOfCake/YesIcanDoThis.java
+++ src/thisIsPeaceOfCake/YesIcanDoThis.java
@@ -6,5 +6,7 @@ public class YesIcanDoThis {
 		// TODO Auto-generated method stub
 System.out.println("I learn this or i will fast for the rest of the night");
 	}
-
+  public void thisIsFunWhenItWorks() {
+	  System.out.println("i will learn it regardless of issues i am facing");
+  }
 }
